e3334e14f0ae303ba46003b7603b4a8af7322f07
aleks <a.kolasinski0203@gmail.com>
2026-09-12 17:18:43 -0700

    style: center course detail skeleton to match real layout

--- src/components/courses/CourseDetailSkeleton.tsx
+++ src/components/courses/CourseDetailSkeleton.tsx
@@ -3,14 +3,15 @@ import { Skeleton } from "@/components/ui/skeleton";
 export function CourseDetailSkeleton() {
   return (
     <div className="flex flex-col gap-6 p-6">
-      <Skeleton className="aspect-video w-full max-w-2xl" />
+      <div className="flex flex-col items-center gap-6">
+        <Skeleton className="aspect-video w-full max-w-2xl" />
 
-      <div className="flex flex-col gap-2">
-        <Skeleton className="h-6 w-1/2" />
-        <Skeleton className="h-4 w-3/4" />
+        <div className="flex flex-col items-center gap-2">
+          <Skeleton className="h-6 w-48" />
+          <Skeleton className="h-4 w-64" />
+          <Skeleton className="mt-2 h-1 w-32" />
+        </div>
       </div>
-
-      <Skeleton className="h-1 max-w-sm" />
 
       <div className="flex flex-col gap-4">
         {Array.from({ length: 3 }).map((_, i) => (
